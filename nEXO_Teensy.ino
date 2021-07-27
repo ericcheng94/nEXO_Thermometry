0137308a072731c564c5b3bdce3f715d034b77b5
Eric Cheng <41352475+ericcheng94@users.noreply.github.com>
2021-07-27 14:19:44 -0700

    Add files via upload

--- nEXO_Teensy.ino
+++ nEXO_Teensy.ino
@@ -7,31 +7,30 @@
 #include <ArduinoRS485.h> // ArduinoModbus depends on the ArduinoRS485 library
 #include <ArduinoModbus.h>
 
-//#define PIN_CS 39
-//const byte charTable[10] = {
-//    B01111110, B00110000, B01101101, B01111001, B00110011, B01011011, B01011111, B01110000, B01111111, B01111011};
-//const byte ledSeg[8] = {
-//    B00001000, B00000111, B00000110, B00000101, B00000100, B00000011, B00000010, B00000001 
-//};
-//#define DECODEMODE_ADDR (0x09)
-//#define BRIGHTNESS_ADDR (0x0A)
-//#define SCANLIMIT_ADDR (0x0B)
-//#define SHUTDOWN_ADDR (0X0C)
-//#define DISPLAYTEST_ADDR (0x0F)
-//#define OP_OFF (0x0)
-//#define OP_ON (0x1)
-//#define BTN_1 37 // Display Push button 1
-//#define BTN_2 35 // Display Push button 2
+#define PIN_CS 37
+const byte charTable[10] = {
+    B01111110, B00110000, B01101101, B01111001, B00110011, B01011011, B01011111, B01110000, B01111111, B01111011};
+const byte ledSeg[8] = {
+    B00001000, B00000111, B00000110, B00000101, B00000100, B00000011, B00000010, B00000001 
+};
+#define DECODEMODE_ADDR (0x09)
+#define BRIGHTNESS_ADDR (0x0A)
+#define SCANLIMIT_ADDR (0x0B)
+#define SHUTDOWN_ADDR (0X0C)
+#define DISPLAYTEST_ADDR (0x0F)
+#define OP_OFF (0x0)
+#define OP_ON (0x1)
+#define BTN_1 36 // Display Push button 1
+#define BTN_2 35 // Display Push button 2
 
 Adafruit_MAX31865 rtd01 = Adafruit_MAX31865(10);
- Adafruit_MAX31865 rtd02 = Adafruit_MAX31865(9);
+Adafruit_MAX31865 rtd02 = Adafruit_MAX31865(9);
 // Adafruit_MAX31865 rtd03 = Adafruit_MAX31865(47);
 // Adafruit_MAX31865 rtd04 = Adafruit_MAX31865(45);
 // Adafruit_MAX31865 rtd05 = Adafruit_MAX31865(43);
 
 // Thermocouple ADC
 Adafruit_MAX31856 tcp01 = Adafruit_MAX31856(8);
-//Adafruit_MAX31856 tcp01 = Adafruit_MAX31856(41);
 // Adafruit_MAX31856 tcp02 = Adafruit_MAX31856(39);
 
 // The value of the Rref resistor. Use 430.0 for PT100 and 4300.0 for PT1000
@@ -106,29 +105,28 @@ void setup() {
   // configure 7 holding registers at address 0x00
   modbusTCPServer.configureHoldingRegisters(0x00, 8);
 
-//  pinMode(PIN_CS, OUTPUT);
-//  pinMode(BTN_1, INPUT);
-//  pinMode(BTN_2, INPUT);
-//  digitalWrite(PIN_CS, HIGH);
-//  SPI.begin(); 
-//  //SPI.beginTransaction(SPISettings(SPIMAXSPEED, MSBFIRST, SPI_MODE0));  
-//  spiWrite(DISPLAYTEST_ADDR, OP_OFF);
-//  //we go into shutdown-mode on startup
-//  spiWrite(SHUTDOWN_ADDR, OP_OFF);
-//  //scanlimit is set to max on startup
-//  spiWrite(SCANLIMIT_ADDR, 7); // show 8 digits
-//  //decode is done in raw mode
-//  spiWrite(DECODEMODE_ADDR, 0);
-//  //clearSegments
-//  spiWrite(SHUTDOWN_ADDR,OP_ON);
-//  // set brightness
-//  spiWrite(BRIGHTNESS_ADDR, 5);
+  pinMode(PIN_CS, OUTPUT);
+  pinMode(BTN_1, INPUT);
+  pinMode(BTN_2, INPUT);
+  digitalWrite(PIN_CS, HIGH);
+  SPI.begin(); 
+  //SPI.beginTransaction(SPISettings(SPIMAXSPEED, MSBFIRST, SPI_MODE0));  
+  spiWrite(DISPLAYTEST_ADDR, OP_OFF);
+  //we go into shutdown-mode on startup
+  spiWrite(SHUTDOWN_ADDR, OP_OFF);
+  //scanlimit is set to max on startup
+  spiWrite(SCANLIMIT_ADDR, 7); // show 8 digits
+  //decode is done in raw mode
+  spiWrite(DECODEMODE_ADDR, 0);
+  //clearSegments
+  spiWrite(SHUTDOWN_ADDR,OP_ON);
+  // set brightness
+  spiWrite(BRIGHTNESS_ADDR, 5);
 }
 
 void loop() {
   EthernetClient client = ethServer.available();
 
-//  modbusTCPServer.accept(client);
   // listen for incoming clients
   if (client) {
      Serial.println("new client");
@@ -139,7 +137,6 @@ void loop() {
         
         if (currentMillis - pollModbusMillis > 1000) {
           // poll for Modbus TCP requests, while client connected
-  //        Serial.println("polling Modbus");
             modbusTCPServer.poll();
             pollModbusMillis = currentMillis;
   
@@ -151,11 +148,10 @@ void loop() {
   //        heartbeat = 1;
   //      }
         }     
-        if (currentMillis - updateSerialMillis > 1000) {
-//          Serial.println("updating Serial");
-          updateSerial();
-          updateSerialMillis = currentMillis;
-        }
+       if (currentMillis - updateSerialMillis > 1000) {
+         updateSerial();
+         updateSerialMillis = currentMillis;
+       }
 //        if (currentMillis - updateRTDMillis > 1000) {
 //          clearALL();
 //          updateRTD(rtd01);
@@ -164,25 +160,21 @@ void loop() {
 //        if (currentMillis - updateDisplayMillis > 1000) {
 //          updateDisplay();
 //          updateDisplayMillis = currentMillis;
-        } 
-        //  if (currentMillis - countMillis > 100) {
-        //    Serial.println("millis");
-        //    countMillis = currentMillis;
-        //  }
-        //  if (digitalRead(BTN_1)) {
-        //    writeV1(555.5);
-        //  } else {
-        //    writeV1(99.9);
-        //  }
-        //  if (digitalRead(BTN_2)) {
-        //    writeSensor("rtd");
-        //  } else {
-        //    writeSensor("tcp");
-        //  }
-     }
+       if (digitalRead(BTN_1)) {
+          writeV1(555.5);
+       } else {
+          writeV1(99.9);
+       }
+//       if (digitalRead(BTN_2)) {
+//          selectSensor("rtd");
+//       } else {
+//          selectSensor("tcp");
+//       }
+     } 
+  }
   // backup for if client disconnects, display still runs
-//  updateDisplay();
-  delay(1000);
+  updateDisplay();
+  delay(250);
 }
 
 
@@ -193,11 +185,10 @@ void  updateSerial() {
   ratio /= 32768;
   Serial.print("Ratio = "); Serial.println(ratio,8);
   Serial.print("Resistance = "); Serial.println(RREF*ratio,8);
-  Serial.print("Temperature = "); Serial.println(rtd01.temperature(RNOMINAL, RREF));
-  Serial.print("Temperature = "); Serial.println(rtd02.temperature(RNOMINAL, RREF));
-  Serial.println();
-  Serial.print("TC Cold Junction: "); Serial.println(tcp01.readCJTemperature());
-  Serial.print("TC Temperature: "); Serial.println(tcp01.readThermocoupleTemperature());
+  Serial.print("rtd01 temperature = "); Serial.println(rtd01.temperature(RNOMINAL, RREF));
+  Serial.print("rtd02 temperature = "); Serial.println(rtd02.temperature(RNOMINAL, RREF));
+  Serial.print("TC Temperature:     "); Serial.println(tcp01.readThermocoupleTemperature());
+  Serial.print("TC Cold Junction:   "); Serial.println(tcp01.readCJTemperature());
 //  Serial.print("TC Fault: "); Serial.println(tcp01.readFault());
   Serial.println();
 }
@@ -214,86 +205,108 @@ void  updateSerial() {
 ////  writeV1(temp);
 //}
 
-//void updateDisplay() {
+void updateDisplay() {
+//  clearALL();
 //    writeV1(23.4);
-//
-//}
+  if (digitalRead(BTN_1)) {
+    selectSensor("rtd", 0);
+  } else {
+    selectSensor("tcp", 0);
+  }
+  if (digitalRead(BTN_2)) {
+    selectSensor("rtd", 1);
+  } else {
+    selectSensor("tcp", 1);
+  }
+}
 
-//void writeV1(float val) {
-//  // if > 999.9 show something else
-//  // Get 100's place
-//  int v100 = (int) val / 100;
-//  int v10  = ((int) val % 100) /10;
-//  int v1   = ((int) val ) % 10;
-//  int v01  = ((int) (val*10) ) %10;
+void writeV1(float val) {
+  // if > 999.9 show something else
+  // Get 100's place
+  int v100 = (int) val / 100;
+  int v10  = ((int) val % 100) /10;
+  int v1   = ((int) val ) % 10;
+  int v01  = ((int) (val*10) ) %10;
 //  Serial.print(v100);
 //  Serial.print(v10);
 //  Serial.print(v1);
 //  Serial.print(".");
 //  Serial.println(v01);
-////  if (v100 == 0){
-////    spiWrite(ledSeg[0], B00000000);
-////  }
-////  else {
-////    spiWrite(ledSeg[0], charTable[v100]);
-////  }
-////  // Write 10s place
-////  spiWrite(ledSeg[1], charTable[v10]);
-////  // Write 1s place
-////  spiWrite(ledSeg[2], B10000000+charTable[v1]);
-////  // Write 0.1s place
-////  spiWrite(ledSeg[3], charTable[v01]);
-////
-////  spiWrite(B00000001, B11111111);
-//
 //  if (v100 == 0){
-//    spiWrite(ledSeg[4], B00000000);
+//    spiWrite(ledSeg[0], B00000000);
 //  }
 //  else {
-//    spiWrite(ledSeg[4], charTable[v100]);
+//    spiWrite(ledSeg[0], charTable[v100]);
 //  }
 //  // Write 10s place
-//  spiWrite(ledSeg[5], charTable[v10]);
+//  spiWrite(ledSeg[1], charTable[v10]);
 //  // Write 1s place
-//  spiWrite(ledSeg[6], B10000000+charTable[v1]);
+//  spiWrite(ledSeg[2], B10000000+charTable[v1]);
 //  // Write 0.1s place
-//  spiWrite(ledSeg[7], charTable[v01]);
-//}
-//void writeSensor(String sensor) {
-//  if (sensor == "rtd") {
-//    spiWrite(ledSeg[0], B00000101);
-//    spiWrite(ledSeg[1], B00001111);
-//    spiWrite(ledSeg[2], B00111101);
-//  } else if (sensor == "tcp") {
-//    spiWrite(ledSeg[0], B00001111);
-//    spiWrite(ledSeg[1], B01001110);
-//    spiWrite(ledSeg[2], B01100111);
-//  }
-//}
-//void clearALL() {
-//  spiWrite(ledSeg[0], B00000000);
-//  spiWrite(ledSeg[1], B00000000);
-//  spiWrite(ledSeg[2], B00000000);
-//  spiWrite(ledSeg[3], B00000000);
-//  spiWrite(ledSeg[4], B00000000);
-//  spiWrite(ledSeg[5], B00000000);
-//  spiWrite(ledSeg[6], B00000000);
-//  spiWrite(ledSeg[7], B00000000);
-//}
-//// Taken from Examples->SPI->BarometricPressureSensor
-//void spiWrite(byte thisRegister, byte thisValue) {
-//  // SCP1000 expects the register address in the upper 6 bits
-//  // of the byte. So shift the bits left by two bits:
-//  //thisRegister = thisRegister << 2;
-//  // now combine the register address and the command into one byte:
-//  //byte dataToSend = thisRegister | WRITE;
-//  // take the chip select low to select the device:
-//  digitalWrite(PIN_CS, LOW);
-//  SPI.transfer(thisRegister); //Send register location
-//  SPI.transfer(thisValue);  //Send value to record into register
-//  // take the chip select high to de-select:
-//  digitalWrite(PIN_CS, HIGH);
-//}
+//  spiWrite(ledSeg[3], charTable[v01]);
+//
+//  spiWrite(B00000001, B11111111);
+
+  if (v100 == 0){
+    spiWrite(ledSeg[4], B00000000);
+  }
+  else {
+    spiWrite(ledSeg[4], charTable[v100]);
+  }
+  // Write 10s place
+  spiWrite(ledSeg[5], charTable[v10]);
+  // Write 1s place
+  spiWrite(ledSeg[6], B10000000+charTable[v1]);
+  // Write 0.1s place
+  spiWrite(ledSeg[7], charTable[v01]);
+}
+void selectSensor(String sensor, int side) {
+  if (side == 0) {
+    if (sensor == "rtd") {
+      spiWrite(ledSeg[0], B00000101);
+      spiWrite(ledSeg[1], B00001111);
+      spiWrite(ledSeg[2], B00111101);
+    } else {
+      spiWrite(ledSeg[0], B00001111);
+      spiWrite(ledSeg[1], B01001110);
+      spiWrite(ledSeg[2], B01100111);
+    }
+  } else {
+    if (sensor == "tcp") {
+      spiWrite(ledSeg[4], B00001111);
+      spiWrite(ledSeg[5], B01001110);
+      spiWrite(ledSeg[6], B01100111);
+    } else {
+      spiWrite(ledSeg[4], B00000101);
+      spiWrite(ledSeg[5], B00001111);
+      spiWrite(ledSeg[6], B00111101);
+    }
+  }
+}
+void clearALL() {
+  spiWrite(ledSeg[0], B00000000);
+  spiWrite(ledSeg[1], B00000000);
+  spiWrite(ledSeg[2], B00000000);
+  spiWrite(ledSeg[3], B00000000);
+  spiWrite(ledSeg[4], B00000000);
+  spiWrite(ledSeg[5], B00000000);
+  spiWrite(ledSeg[6], B00000000);
+  spiWrite(ledSeg[7], B00000000);
+}
+// Taken from Examples->SPI->BarometricPressureSensor
+void spiWrite(byte thisRegister, byte thisValue) {
+  // SCP1000 expects the register address in the upper 6 bits
+  // of the byte. So shift the bits left by two bits:
+  //thisRegister = thisRegister << 2;
+  // now combine the register address and the command into one byte:
+  //byte dataToSend = thisRegister | WRITE;
+  // take the chip select low to select the device:
+  digitalWrite(PIN_CS, LOW);
+  SPI.transfer(thisRegister); //Send register location
+  SPI.transfer(thisValue);  //Send value to record into register
+  // take the chip select high to de-select:
+  digitalWrite(PIN_CS, HIGH);
+}
 
 void checkPTFault() {
   // Check and print any faults

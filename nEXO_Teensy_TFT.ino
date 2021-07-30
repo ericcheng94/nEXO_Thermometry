8c4d270f6f8bc1f865c6a4eb3593be4d855cee51
Eric Cheng <41352475+ericcheng94@users.noreply.github.com>
2021-07-30 15:11:43 -0700

    Add files via upload

---
+++ nEXO_Teensy_TFT.ino
@@ -0,0 +1,291 @@
+#include <Adafruit_MAX31865.h>
+#include <Adafruit_MAX31856.h>
+#include <Wire.h>
+#include <SPI.h>
+#include "ILI9341_t3.h"
+#include <NativeEthernet.h>
+#include <Arduino.h>
+#include <ArduinoRS485.h> // ArduinoModbus depends on the ArduinoRS485 library
+#include <ArduinoModbus.h>
+
+Adafruit_MAX31865 rtd01 = Adafruit_MAX31865(10);
+Adafruit_MAX31865 rtd02 = Adafruit_MAX31865(36);
+// Adafruit_MAX31865 rtd03 = Adafruit_MAX31865(47);
+// Adafruit_MAX31865 rtd04 = Adafruit_MAX31865(45);
+// Adafruit_MAX31865 rtd05 = Adafruit_MAX31865(43);
+
+// Thermocouple ADC
+Adafruit_MAX31856 tcp01 = Adafruit_MAX31856(35);
+// Adafruit_MAX31856 tcp02 = Adafruit_MAX31856(39);
+
+// The value of the Rref resistor. Use 430.0 for PT100 and 4300.0 for PT1000
+#define RREF      430.0
+// The 'nominal' 0-degrees-C resistance of the sensor
+// 100.0 for PT100, 1000.0 for PT1000
+#define RNOMINAL  100.0
+
+#define TFT_DC  9
+#define TFT_CS 37
+ILI9341_t3 tft = ILI9341_t3(TFT_CS, TFT_DC);
+
+// Enter a MAC address for your controller below.
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+// The IP address will be dependent on your local network:
+IPAddress ip(192, 168, 0, 242);
+
+EthernetServer ethServer(502);
+ModbusTCPServer modbusTCPServer;
+
+uint32_t currentMillis;
+uint32_t pollModbusMillis;
+uint32_t updateSensorsMillis;
+uint32_t updateClientMillis;
+uint32_t updateSerialMillis;
+uint32_t updateDisplayMillis;
+//uint32_t display1Millis;
+//uint32_t display2Millis;
+unsigned int  heartbeat;
+
+// Global variables to make current sensor temps available to every function
+float rtd01temp;
+float rtd02temp;
+float rtd03temp;
+float rtd04temp;
+float rtd05temp;
+float tcp01temp;
+float tcp02temp;
+
+//Modbus holding register address mapping
+const int rtd01_reg = 0x00;
+const int rtd02_reg = 0x01;
+const int rtd03_reg = 0x02;
+const int rtd04_reg = 0x03;
+const int rtd05_reg = 0x04;
+const int tcp01_reg1 = 0x05;
+const int tcp01_reg2 = 0x06;
+const int tcp02_reg1 = 0x07;
+const int tcp02_reg2 = 0x08;
+const int tick_tock = 0x09;
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  Serial.println("nEXO ModbusTCP RTDTest!");
+  rtd01.begin(MAX31865_3WIRE);  // set to 2WIRE or 4WIRE as necessary
+  rtd02.begin(MAX31865_3WIRE);  // set to 2WIRE or 4WIRE as necessary
+//  rtd03.begin(MAX31865_3WIRE);  // set to 2WIRE or 4WIRE as necessary
+//  rtd04.begin(MAX31865_3WIRE);  // set to 2WIRE or 4WIRE as necessary
+//  rtd05.begin(MAX31865_3WIRE);  // set to 2WIRE or 4WIRE as necessary
+  tcp01.begin();
+  tcp01.setThermocoupleType(MAX31856_TCTYPE_T);
+//  tcp02.begin();
+//  tcp02.setThermocoupleType(MAX31856_TCTYPE_T);
+  tft.begin();
+//  Serial.print("TC Type: "); Serial.println(tcp01.getThermocoupleType());
+
+  Ethernet.begin(mac, ip);
+
+  // Check for Ethernet hardware present
+  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+    Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+    while (true) {
+      delay(1); // do nothing, no point running without Ethernet hardware
+    }
+  }
+  // Keeps displaying error despite having thernet connection, commented out for now
+  // if (Ethernet.linkStatus() == LinkOFF) {
+  //   Serial.println("Ethernet cable is not connected.");
+  // }
+
+  // start the server
+  ethServer.begin();
+
+  // start the Modbus TCP server
+  if (!modbusTCPServer.begin()) {
+    Serial.println("Failed to start Modbus TCP Server!");
+    while (1);
+  }
+
+  // configure 7 holding registers at address 0x00
+  modbusTCPServer.configureHoldingRegisters(0x00, 10);
+
+  tft.fillScreen(ILI9341_BLACK);
+  tft.setTextColor(ILI9341_WHITE);
+  tft.setTextSize(3);
+  tft.setRotation(3);
+  tft.setCursor(10, 10);
+  tft.println("Thermometry v1.0");
+  tft.fillScreen(ILI9341_BLACK);
+}
+
+void loop() {
+  EthernetClient client = ethServer.available();
+
+  // listen for incoming clients
+  if (client) {
+     Serial.println("new client");
+     modbusTCPServer.accept(client);
+  
+     while (client.connected()) {
+       currentMillis = millis();
+        
+       if (currentMillis - pollModbusMillis > 1000) {
+         // poll for Modbus TCP requests, while client connected
+         modbusTCPServer.poll();
+         pollModbusMillis = currentMillis;
+  
+//         if (heartbeat > 65000) {
+//           heartbeat = 1;
+//         } else if (heartbeat % 2 == 1) {
+//           modbusTCPServer.holdingRegisterWrite(tick_tock, 1);
+//           heartbeat = 0;
+//         } else {
+//           modbusTCPServer.holdingRegisterWrite(tick_tock, 0);
+//           heartbeat = 1;
+//         }
+       }     
+
+       if (currentMillis - updateSensorsMillis > 100) {
+         rtd01temp = rtd01.temperature(RNOMINAL, RREF);
+         rtd02temp = rtd02.temperature(RNOMINAL, RREF);
+//         rtd03temp = rtd03.temperature(RNOMINAL, RREF);
+//         rtd04temp = rtd04.temperature(RNOMINAL, RREF);
+//         rtd05temp = rtd05.temperature(RNOMINAL, RREF);
+         tcp01temp = tcp01.readThermocoupleTemperature();         
+//         tcp02temp = tcp02.readThermocoupleTemperature();         
+         updateSensorsMillis = currentMillis;
+       }
+       
+       if (currentMillis - updateClientMillis > 1000) {
+         updateClient(tcp01temp);
+//         updateClient(tcp01temp, tcp02temp);
+         updateClientMillis = currentMillis;
+       }      
+        
+       if (currentMillis - updateSerialMillis > 2000) {
+         updateSerial();
+         updateSerialMillis = currentMillis;
+       }
+
+       if (currentMillis - updateDisplayMillis > 1000) {
+         updateDisplay(rtd01temp, rtd02temp, tcp01temp);
+//  updateDisplay(rtd01temp, rtd02temp, rtd03temp, rtd04temp, rtd05temp, tcp01temp, tcp02temp);
+         updateDisplayMillis = currentMillis;
+       }
+     }
+  }
+  // backup for if client disconnects, display still runs
+  rtd01temp = rtd01.temperature(RNOMINAL, RREF);
+  rtd02temp = rtd02.temperature(RNOMINAL, RREF);
+//         rtd03temp = rtd03.temperature(RNOMINAL, RREF);
+//         rtd04temp = rtd04.temperature(RNOMINAL, RREF);
+//         rtd05temp = rtd05.temperature(RNOMINAL, RREF);
+  tcp01temp = tcp01.readThermocoupleTemperature();         
+//         tcp02temp = tcp02.readThermocoupleTemperature();   
+  updateDisplay(rtd01temp, rtd02temp, tcp01temp);
+//  updateDisplay(rtd01temp, rtd02temp, rtd03temp, rtd04temp, rtd05temp, tcp01temp, tcp02temp);
+  delay(250); // TODO Change to millis
+}
+
+union {
+    float asFloat;
+    uint16_t asInt[2];
+} flreg;
+
+void updateClient(float tcp01temp) {
+  uint16_t rawRTD01 = rtd01.readRTD();
+  uint16_t rawRTD02 = rtd02.readRTD();
+//  Serial.print("rtd01 raw = "); Serial.println(rawRTD01);
+//  Serial.print("rtd02 raw = "); Serial.println(rawRTD02);
+//  Serial.print("tcp temperature = "); Serial.println(tcp01temp);
+  modbusTCPServer.holdingRegisterWrite(rtd01_reg, rawRTD01);
+  modbusTCPServer.holdingRegisterWrite(rtd02_reg, rawRTD02);
+
+  flreg.asFloat = tcp01temp;
+  modbusTCPServer.holdingRegisterWrite(tcp01_reg1, flreg.asInt[1]);
+  modbusTCPServer.holdingRegisterWrite(tcp01_reg2, flreg.asInt[0]);
+}
+
+void  updateSerial() {
+  uint16_t rtd = rtd01.readRTD();
+  Serial.print("RTD value: "); Serial.println(rtd);
+  float ratio = rtd;
+  ratio /= 32768;
+  Serial.print("Ratio = "); Serial.println(ratio,8);
+  Serial.print("Resistance = "); Serial.println(RREF*ratio,8);
+  Serial.print("rtd01 temperature = "); Serial.println(rtd01.temperature(RNOMINAL, RREF));
+  Serial.print("rtd02 temperature = "); Serial.println(rtd02.temperature(RNOMINAL, RREF));
+  Serial.print("TC Temperature:     "); Serial.println(tcp01.readThermocoupleTemperature());
+  Serial.print("TC Cold Junction:   "); Serial.println(tcp01.readCJTemperature());
+//  Serial.print("TC Fault: "); Serial.println(tcp01.readFault());
+  Serial.println();
+}
+
+void updateDisplay(float rtd01temp, float rtd02temp, float tcp01temp) {
+  rtd01temp += 273.15;
+  rtd02temp += 273.15;
+  tcp01temp += 273.15;
+
+
+  if (rtd01temp < 300)
+    tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
+  else
+    tft.setTextColor(ILI9341_RED, ILI9341_BLACK);
+  tft.setCursor(10, 10);  
+  tft.print("RTD_01:"); tft.print(rtd01temp); tft.println(" K");
+
+  if (rtd02temp < 300)
+    tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
+  else
+    tft.setTextColor(ILI9341_RED, ILI9341_BLACK);  
+  tft.setCursor(10, 40);
+  tft.print("RTD_02:"); tft.print(rtd02temp); tft.println(" K");
+
+  tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
+  tft.setCursor(10, 70);
+  tft.print("RTD_01:"); tft.print("--"); tft.println(" K");
+  tft.setCursor(10, 100);
+  tft.print("RTD_01:"); tft.print("--"); tft.println(" K");
+  tft.setCursor(10, 130);
+  tft.print("RTD_01:"); tft.print("--"); tft.println(" K");
+  
+  if (tcp01temp < 300)
+    tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
+  else
+    tft.setTextColor(ILI9341_RED, ILI9341_BLACK);  
+  tft.setCursor(10, 170);
+  tft.print("TCP_01:"); tft.print(tcp01temp); tft.println(" K");
+
+  tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
+  tft.setCursor(10, 200);
+  tft.print("TCP_01:"); tft.print("--"); tft.println(" K");
+}
+
+void checkPTFault() {
+  // Check and print any faults
+  uint8_t fault = rtd01.readFault();
+  if (fault) {
+    Serial.print("Fault 0x"); Serial.println(fault, HEX);
+    if (fault & MAX31865_FAULT_HIGHTHRESH) {
+      Serial.println("RTD High Threshold");
+    }
+    if (fault & MAX31865_FAULT_LOWTHRESH) {
+      Serial.println("RTD Low Threshold");
+    }
+    if (fault & MAX31865_FAULT_REFINLOW) {
+      Serial.println("REFIN- > 0.85 x Bias");
+    }
+    if (fault & MAX31865_FAULT_REFINHIGH) {
+      Serial.println("REFIN- < 0.85 x Bias - FORCE- open");
+    }
+    if (fault & MAX31865_FAULT_RTDINLOW) {
+      Serial.println("RTDIN- < 0.85 x Bias - FORCE- open");
+    }
+    if (fault & MAX31865_FAULT_OVUV) {
+      Serial.println("Under/Over voltage");
+    }
+    rtd01.clearFault();
+  }
+}
